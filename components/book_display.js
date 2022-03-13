ae9d148348391b23951c71bba2e017955fe75f06
aminoa <28660350+aminoa@users.noreply.github.com>
2022-03-13 00:22:18 -0500

    broke homepage

--- components/book_display.js
+++ components/book_display.js
@@ -1,51 +1,61 @@
 import Book from "../components/book.js";
-import React from 'react';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import Grid from "@mui/material/Grid";
 
-class BookDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-    }
+export default class BookDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    // var books = JSON.stringify(props.books);
+    this.state = {books: []}
+    // for (var i = 0; i < this.props.books.length; i++) {
+    //   this.state[i] = this.props.books[i];
+    // }
 
-    render() {
-        return (
-        <Grid container spacing={2}>
+  }
+
+    CreateBookList() {
+        let books = [];
+        var height = "300";
+        var width = "180";
+
+        for (var i = 0; i < this.state.length; ++i) {
+            books.push(
             <Grid item xs={2}>
-            <Book 
-                name="The Queen of Paris: A Novel of Coco Chanel" 
-                author="Sonic"
-                returnDate="Test Date" 
-                status="In stock" 
-                url="https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?UserID=NYPL49807&Password=CC68707&Return=1&Type=L&Value=9781982546939&erroroverride=1" 
-                description="Test description"
-                height="300"
-                width="180"
-            />
+                <Book
+                name={this.state[i].name}
+                author={this.state[i].author}
+                returnDate={this.state[i].returnDate}
+                status={this.state[i].status}
+                imageURL={this.state[i].imageURL}
+                description={this.state[i].description}
+                height={height}
+                width={width}
+                />
             </Grid>
-            <Grid item xs={2}>
-            <Book 
-                name="The Queen of Paris: A Novel of Coco Chanel" 
-                author="Sonic"
-                returnDate="Test Date" 
-                status="In stock" 
-                url="https://contentcafe2.btol.com/ContentCafe/Jacket.aspx?UserID=NYPL49807&Password=CC68707&Return=1&Type=L&Value=9781982546939&erroroverride=1" 
-                description="Test description"
-                height="300"
-                width="180"
-            />
-            </Grid>
-        </Grid>
-        )    
-    }
-}
-
-export async function getServerSideProps(context) {
-    return {
-        props: {
-            
+            );
         }
-    }
-}
+    };
 
-export default BookDisplay;
+  render() {
+    var height = "300";
+    var width = "180";
+
+    return (
+      <Grid container spacing={2}>
+        {this.CreateBookList()}
+        {/* <Grid item xs={2}>
+          <Book
+            name={this.props.name}
+            author={this.props.author}
+            returnDate={this.props.returnDate}
+            status={this.props.status}
+            imageURL={this.props.imageURL}
+            description={this.props.description}
+            height={height}
+            width={width}
+          />
+        </Grid> */}
+      </Grid>
+    );
+  }
+}
